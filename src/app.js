0218bd7f7787520e5d06058430d08429480088bd
puppalah <harika.sai.rama.lakshmi.puppala@adp.com>
2023-05-16 23:16:17 +0530

    fix issue

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express    = require('express')
 const app        = express()
 const Route      = require('./Router/index')
-const SERVERPORT = process.env.SERVERPORT || 3000
+const SERVERPORT =  3000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
